a222b633302a0948d698d624609d5a1f1833e4a6
Skyler Hartle <skyler@shapesocial.net>
2017-10-26 14:35:06 -0700

    removed console logs, oops

--- src/js/zendesk_sdk.js
+++ src/js/zendesk_sdk.js
@@ -9,29 +9,4 @@ zendesk.initializeApp = function() {
   });
 };
 
-
-//old, from modal version
-zendesk.generateModal = function() {
-    client.on('app.registered', function(context) {
-      console.log(context);
-        if (context.context.location === 'background') {
-            client.invoke('instances.create', {
-                location: 'modal',
-                url: 'assets/iframe.html'
-            }).then(function(modalContext) {
-                console.log('fired off the modal successfully');
-                var modalClient = client.instance(modalContext['instances.create'][0].instanceGuid);
-                modalClient.invoke('resize', {
-                    width: '400px',
-                    height: '350px'
-                });
-
-                modalClient.on('modal.close', function() {
-                    console.log('user just closed the modal');
-                });
-            });
-        }
-    });
-};
-
 module.exports = zendesk;
